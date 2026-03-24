f89806415e981a8d3aeebdda0eb6714c39d4230b
Sava2901 <savasergiu2006@gmail.com>
2026-03-24 15:44:49 +0200

    feat: implement product entity

---
+++ src/main/java/org/endava/onlineshop/model/entities/Product.java
@@ -0,0 +1,35 @@
+package org.endava.onlineshop.model.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @Column(name = "id",  nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @OneToMany
+    @Column(name = "productImage", nullable = false)
+    private List<ProductImage> image = new ArrayList<ProductImage>();
+}
