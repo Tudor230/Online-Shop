7c5e733caa7cb3d9421444bbf98224055d406ba4
Tudor230 <133231902+Tudor230@users.noreply.github.com>
2026-04-21 14:38:02 +0300

    feat: add ProductRepository for product data access

---
+++ backend/src/main/java/org/endava/onlineshop/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package org.endava.onlineshop.repository;
+
+import org.endava.onlineshop.model.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByName(String name);
+    List<Product> findByPriceBetween(Double priceStart, Double priceEnd);
+    List<Product> findByPriceLessThan(Double price);
+    List<Product> findByPriceGreaterThan(Double price);
+}
