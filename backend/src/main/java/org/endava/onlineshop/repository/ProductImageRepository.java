812ba3bca2765943ee9a96acf2910d05cec0c98a
Paul Soporan <paul.soporan@gmail.com>
2026-04-21 14:40:15 +0300

    feat: product image repository

---
+++ backend/src/main/java/org/endava/onlineshop/repository/ProductImageRepository.java
@@ -0,0 +1,12 @@
+package org.endava.onlineshop.repository;
+
+import org.endava.onlineshop.model.entities.ProductImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
+    List<ProductImage> findByProductId(Long productId);
+}
