59a69c734980cd2089e41b67f457f339e8bca691
Muntean Darius <munteandarius2005@gmail.com>
2026-04-21 14:37:43 +0300

    feat: Created user repository with find by email

---
+++ backend/src/main/java/org/endava/onlineshop/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package org.endava.onlineshop.repository;
+
+import org.endava.onlineshop.model.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByEmail(String email);
+}
