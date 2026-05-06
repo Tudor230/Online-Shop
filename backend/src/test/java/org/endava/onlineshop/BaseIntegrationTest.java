1cb20124da67bc83bc649fa63e495b7c6d548a9c
Paul Soporan <paul.soporan@gmail.com>
2026-05-06 22:22:54 +0300

    test: use mocks

--- backend/src/test/java/org/endava/onlineshop/BaseIntegrationTest.java
+++ backend/src/test/java/org/endava/onlineshop/BaseIntegrationTest.java
@@ -1,7 +1,11 @@
 package org.endava.onlineshop;
 
+import org.endava.onlineshop.security.KeycloakAdminService;
+import org.springframework.ai.google.genai.GoogleGenAiEmbeddingConnectionDetails;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -9,6 +13,14 @@ import org.testcontainers.containers.PostgreSQLContainer;
 @SpringBootTest
 @Testcontainers
 public abstract class BaseIntegrationTest {
+    @MockitoBean
+    private JwtDecoder jwtDecoder;
+
+    @MockitoBean
+    private KeycloakAdminService keycloakAdminService;
+
+    @MockitoBean
+    private GoogleGenAiEmbeddingConnectionDetails googleGenAiEmbeddingConnectionDetails;
 
     @Container
     @ServiceConnection
